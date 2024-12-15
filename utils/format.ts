a73b17afb1dc08da8301d53d6ed9a9cf7d64556f
Olivier Dounla <olidounla@gmail.com>
2024-12-14 19:04:46 -0800

    change on format for curencies

--- utils/format.ts
+++ utils/format.ts
@@ -1,14 +1,7 @@
-export const formatCurrency = (
-  amount: number | null,
-  currency: "USD" | "CFA" = "USD"
-) => {
+export const formatCurrency = (amount: number | null) => {
   const value = amount || 0;
-  const options = {
+  return new Intl.NumberFormat("en-US", {
     style: "currency",
-    currency: currency === "USD" ? "USD" : "XAF", // 'XAF' is the ISO code for CFA Francs
-  };
-
-  const locale = currency === "USD" ? "en-US" : "fr-FR"; // Use locale for correct formatting
-
-  return new Intl.NumberFormat(locale, options).format(value);
+    currency: "USD",
+  }).format(value);
 };
